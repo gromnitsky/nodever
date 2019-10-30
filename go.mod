1432322d20c0e4b391144134da7b0281c090a190
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2019-10-30 21:07:50 +0200

    switch to go modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gromnitsky/nodever
+
+go 1.13
+
+require github.com/mattn/go-shellwords v1.0.6
--- meta/meta.go
+++ meta/meta.go
@@ -1,4 +1,4 @@
 package meta
 
-var Version = "0.1.0"
+var Version = "0.1.1"
 var Website = "https://github.com/gromnitsky/nodever"
